21005d638c492e62e6d852ff27d081b41dda077a
Asmita <asmita2018fzd@gmail.com>
2024-12-27 23:23:06 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,11 @@ import AuthProvider from "../context/AuthProvider";
 import { Toaster } from "react-hot-toast";
 
 
+export const metadata = {
+  title: "DevConnect",
+  description: "A social media platform for developers",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
