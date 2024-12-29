6cee1f0221c2d60808ac63fe6eb11c3230dc520d
Asmita <asmita2018fzd@gmail.com>
2024-12-29 21:41:28 +0530

    icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
 import "./globals.css";
 import AuthProvider from "../context/AuthProvider";
 import { Toaster } from "react-hot-toast";
+import { icons } from "@tabler/icons-react";
 
 
 export const metadata = {
   title: "DevConnect",
   description: "A social media platform for developers",
+  icons: {
+    favicon: "/public/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
